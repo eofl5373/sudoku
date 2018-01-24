d1c6a2eb7b7f160e356f161bd82dfcdcf9767a77
Administrator <Administrator@WIN-BHHANT8D3EH>
2018-01-25 02:17:03 +0900

    Mapper 동작 되도록 수정

--- bnk/src/main/java/com/bnk/plus/service/notice/persistence/NoticeMapper.java
+++ bnk/src/main/java/com/bnk/plus/service/notice/persistence/NoticeMapper.java
@@ -2,8 +2,10 @@ package com.bnk.plus.service.notice.persistence;
 
 import java.util.List;
 
+import com.bnk.plus.config.support.Master;
 import com.bnk.plus.entity.Board;
 
+@Master
 public interface NoticeMapper {
 	List<Board> getNoticeList(Board board);
 }
